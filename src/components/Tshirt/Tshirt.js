67b63277d5db49a9efd6432195c877d2c88ddfd0
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-01 16:00:05 +0600

    style tshirt components

--- src/components/Tshirt/Tshirt.js
+++ src/components/Tshirt/Tshirt.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Tshirt.css';
 
 const Tshirt = ({ tshirt }) => {
-    const { name, picture } = tshirt;
+    const { name, picture, price, id } = tshirt;
     return (
-        <div>
+        <div className='tshirt'>
             <img src={picture} alt="" />
-            <p>Name: {name}</p>
+            <h4>Name: {name}</h4>
+            <h6>Price: {price}</h6>
+            <button>Add to Cart</button>
         </div>
     );
 };

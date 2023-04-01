2133dc14ab796a890bb062f8f71bd4660c230bf2
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-01 21:43:17 +0600

    explore context api

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './Grandpa.css';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 
+const HouseContext = createContext('10 ta house');
+
 const Grandpa = () => {
     const house = 7;
     return (
-        <div className='grandpa'>
-            <h2>Grandpa</h2>
+        <HouseContext.Provider value='Dadu house'>
+            <div className='grandpa'>
+                <h2>Grandpa</h2>
 
-            <section className='flex'>
-                <Father house={house}/> 
-                <Uncle house={house}/> 
-                <Aunty house={house}/> 
-            </section>
-        </div>
+                <section className='flex'>
+                    <Father house={house} />
+                    <Uncle house={house} />
+                    <Aunty house={house} />
+                </section>
+            </div>
+        </HouseContext.Provider>
     );
 };
 

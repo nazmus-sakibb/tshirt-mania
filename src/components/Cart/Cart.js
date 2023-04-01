05169725bedf7257f670c1bdc0dc11585b55ea1e
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-01 20:40:55 +0600

    conditional rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 const Cart = ({ cart, handleRemoveItem }) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>Please buy at least one item!</p>;
+    }
+    else if(cart.length === 1){
+        message = <div>
+            <h3>Amar jonno ekta neo</h3>
+            <p>Tomar jonno ekta neo</p>
+            <p><small>tar jonno ekta neo</small></p>
+        </div>
+    }
+    else {
+        message = <p>Thanks for buying</p>;
+    }
+
     return (
         <div>
             <h3>Order Summary</h3>
@@ -11,8 +26,21 @@ const Cart = ({ cart, handleRemoveItem }) => {
                     <button onClick={() => handleRemoveItem(tshirt)}>X</button>
                 </p>)
             }
+            {
+                message
+            }
+            {cart.length === 3 ? <p>3 jon k gift diba?</p> : <p>buy more or less</p>}
+            <p>&& operator</p>
+            {
+                cart.length === 2 && <h2>Double Items</h2>
+            }
         </div>
     );
 };
 
 export default Cart;
+
+
+/* 
+
+*/

a3325e7bd12c30012504aa6dc2106ad7ece62443
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-01 15:37:42 +0600

    children route and nav link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Main from './layouts/Main';
 import Home from './components/Home/Home';
+import Orders from './components/Orders/Orders';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home/>
+      element: <Main />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/orders',
+          element: <Orders />
+        }
+      ]
     }
   ]);
 
   return (
     <div className="App">
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/orders'>Orders</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/inventory'>Inventory</Link>
+        </nav>
     );
 };
 
